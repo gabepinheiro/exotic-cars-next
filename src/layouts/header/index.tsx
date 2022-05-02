6c7c1082b7f566221734117e9326d4c66aef11d4
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-05-02 10:39:27 -0300

    feat(layouts/header): adding menu hamburguer icon

--- src/layouts/header/index.tsx
+++ src/layouts/header/index.tsx
@@ -1,4 +1,5 @@
 import { Logo, Button, SearchBar } from '~/components'
+import { AiOutlineMenu as MenuIcon } from 'react-icons/ai'
 import { ContainerContent } from '~/layouts'
 
 import * as S from './styles'
@@ -6,6 +7,7 @@ import * as S from './styles'
 export const Header = () => (
   <S.HeaderWrapper>
     <ContainerContent>
+      <MenuIcon size={24} />
       <Logo />
       <SearchBar />
       <S.ButtonsWrapper>
