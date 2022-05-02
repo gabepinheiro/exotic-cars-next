60b5160b5dc6cfe3229d46250a1b52a439f3d816
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-05-02 15:51:09 -0300

    feat(pages/home): requesting car data and rendering car cards

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,43 @@
+import { GetStaticProps } from 'next'
+import { client } from '~/graphql/client'
+import { GetCarsQuery } from '~/graphql/generated/graphql'
+import { GET_CARS } from '~/graphql/queries'
+import { Car } from '~/resources'
 import { HomeTemplate } from '~/templates/home'
 
-const Home = () => (
-  <HomeTemplate />
+type HomeProps = {
+  cars: Car[]
+}
+
+const Home = ({ cars }: HomeProps) => (
+  <HomeTemplate cars={cars} />
 )
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const { cars } = await client.request<GetCarsQuery>(GET_CARS)
+
+  const transformedCars = cars.map((car) => ({
+    id: car.id,
+    brand: car.brand.toString(),
+    model: car.model,
+    slug: car.slug,
+    brandImage: car.brandImage.url,
+    thumb: car.thumb.url,
+    colors: car.colors.map(({ image, color }) => ({
+      image: image.url,
+      color,
+    })),
+    rent: {
+      amount: car.rent.amount,
+      per: car.rent.per,
+    },
+  }))
+
+  return {
+    props: {
+      cars: transformedCars,
+    },
+  }
+}
 
 export default Home
--- src/templates/home/index.tsx
+++ src/templates/home/index.tsx
@@ -1,58 +1,20 @@
 import Head from 'next/head'
 import { CardCard } from '~/components'
+import { Car } from '~/resources'
 
 import * as S from './styles'
 
-const cars = [
-  {
-    brand: 'Ferrari',
-    model: 'California',
-    brandImage: '/images/brand.png',
-    thumb: '/images/thumb.png',
-    rent: {
-      amount: 725,
-      per: 'day',
-    },
-  },
-  {
-    brand: 'Ferrari',
-    model: 'California',
-    brandImage: '/images/brand.png',
-    thumb: '/images/thumb.png',
-    rent: {
-      amount: 725,
-      per: 'day',
-    },
-  },
-  {
-    brand: 'Ferrari',
-    model: 'California',
-    brandImage: '/images/brand.png',
-    thumb: '/images/thumb.png',
-    rent: {
-      amount: 725,
-      per: 'day',
-    },
-  },
-  {
-    brand: 'Ferrari',
-    model: 'California',
-    brandImage: '/images/brand.png',
-    thumb: '/images/thumb.png',
-    rent: {
-      amount: 725,
-      per: 'day',
-    },
-  },
-]
+type HomeTemplateProps = {
+  cars: Car[]
+}
 
-export const HomeTemplate = () => (
+export const HomeTemplate = ({ cars }: HomeTemplateProps) => (
   <>
     <Head>
       <title>Exotic Cars</title>
     </Head>
     <S.Wrapper>
-      {cars.map((car, index) => <CardCard key={index} {...car} />)}
+      {cars.map(({ id, ...props }) => <CardCard key={id} {...props} />)}
     </S.Wrapper>
   </>
 )
