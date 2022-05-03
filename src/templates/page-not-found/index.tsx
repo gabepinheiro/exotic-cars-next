0defb82db1de18c5fc2d7b39a73ee1282ea507b5
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-05-03 16:58:38 -0300

    feat(templates/page-not-found): adding Head component and title

--- src/templates/page-not-found/index.tsx
+++ src/templates/page-not-found/index.tsx
@@ -1,8 +1,12 @@
+import Head from 'next/head'
 import { ButtonLink } from '~/components'
 import * as S from './styles'
 
 export const PageNotFoundTemplate = () => (
   <S.Wrapper>
+    <Head>
+      <title>Página não encontrada.</title>
+    </Head>
     <S.Heading>Page not found.</S.Heading>
     <ButtonLink href='/' reverse='true'>Back home page</ButtonLink>
   </S.Wrapper>
