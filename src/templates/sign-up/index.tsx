f91ca43753c2c57ee7d065b585f4c2cfeaf0296c
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-05-04 14:33:21 -0300

    feat(templates/sign-up): adding Head and title

--- src/templates/sign-up/index.tsx
+++ src/templates/sign-up/index.tsx
@@ -1,8 +1,12 @@
+import Head from 'next/head'
 import { Form, Center, Flex } from '~/components'
 import * as S from './styles'
 
 export const SignUpTemplate = () => (
   <S.Wrapper>
+    <Head>
+      <title>Sign up</title>
+    </Head>
     <Center>
       <Flex column alignItems='center' gap={64}>
         <S.Heading>Sign Up</S.Heading>
