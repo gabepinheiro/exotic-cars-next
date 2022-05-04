26c790224ea2b46732688f416882280518fe86ee
Gabriel Pinheiro <gabrielpsilvas@live.com>
2022-05-04 14:30:54 -0300

    feat(templates/sign-in): adding Head and title

--- src/templates/sign-in/index.tsx
+++ src/templates/sign-in/index.tsx
@@ -1,9 +1,13 @@
+import Head from 'next/head'
 import { Form, Center, Flex } from '~/components'
 
 import * as S from './styles'
 
 export const SignInTemplate = () => (
   <S.Wrapper>
+    <Head>
+      <title>Sign in</title>
+    </Head>
     <Center>
       <Flex column alignItems='center' gap={64}>
         <S.Heading>Sign In</S.Heading>
